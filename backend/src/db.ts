f451e5849ea21e2c12b70da23ad2245287155701
emomo81 <emmanuelmomo81@gmail.com>
2026-04-24 22:16:10 +0200

    Update screening routes

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,6 +1,7 @@
 import mongoose, { Connection } from 'mongoose';
 import config from './config';
 
+
 export async function connectDb(): Promise<Connection> {
   mongoose.set('strictQuery', true);
   await mongoose.connect(config.mongodbUri);
